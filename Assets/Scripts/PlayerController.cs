a036ac505acbcd612200ef8edde291282aef8dac
irfanshah22 <45657454+irfanshah22@users.noreply.github.com>
2024-03-26 16:42:48 +0500

    controoler extra things deleted

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,12 +24,9 @@ public class PlayerController : MonoBehaviour
     public bool isDragging;
     public float dragThreshold = 0.1f; // Adjust as needed
 
-
-     // Start is called before the first frame update yup
-
-
-
-
+      // Start is called before the first frame update yup
+ 
+ 
     private void Awake()
     {
         Instance = this;
@@ -157,93 +154,6 @@ public class PlayerController : MonoBehaviour
               Debug.Log("Stopped Dragging");
         }
     }
-
-
-
-    void PlayerMovement()
-    {
-        Vector3 camRight = Cameraman.transform.right;
-        Vector3 camForward = Cameraman.transform.forward;
-        camRight.y = 0;
-        camForward.y = 0;
-         Vector3 move = camRight.normalized * Input.GetAxis("Horizontal") + camForward.normalized * Input.GetAxis("Vertical");
-        cc.Move(move.normalized * Time.deltaTime * 10 * Speed * ((Input.GetKey(KeyCode.LeftShift)?2:1)));
-        if(move.magnitude > 0) transform.forward = move.normalized;
-
-        GravityForce -= Gravity * Time.deltaTime * 5;
-        if(isGround.collider && GravityForce < -2) GravityForce = -2;
-        else if(GravityForce < -99) GravityForce = -99;
-        cc.Move(new Vector3(0, GravityForce * Time.deltaTime, 0));
-
-        if (inWater) ripple.gameObject.SetActive(true);
-        else ripple.gameObject.SetActive(false);
-        Physics.Raycast(transform.position, Vector3.down, out isGround, 2.7f, LayerMask.GetMask("Ground"));
-        Debug.DrawRay(transform.position, Vector3.down* 2.7f);
-
-        //
-        float height = cc.height + cc.radius;
-        inWater = Physics.Raycast(transform.position + Vector3.up * height, Vector3.down, height*2, LayerMask.GetMask("Water"));
-        Debug.DrawRay(transform.position + Vector3.up * height, Vector3.down* height);
-    }
-    void Jumping()
-    {
-        // || !isGround.collider
-        if(GravityForce > 2) return;
-        float JumpMutify = 2;
-        GravityForce = Mathf.Sqrt(JumpHeight * Gravity * JumpMutify);
-        cc.Move(new Vector3(0, GravityForce * Time.deltaTime, 0));
-    }
-    void CameraControl()
-    {
-        Cameraman.transform.position = transform.position;
-        CameraY -= Input.GetAxis("Mouse Y") * Time.fixedDeltaTime * 300;
-        CameraY = Mathf.Clamp(CameraY, -45, 45);
-
-        Zoom += Input.mouseScrollDelta.y * Time.fixedDeltaTime * 100;
-        Zoom = Mathf.Clamp(Zoom, -12, -4);
-
-        Cameraman.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.fixedDeltaTime * 150, 0);
-        Cameraman.transform.eulerAngles = new Vector3(CameraY, Cameraman.transform.eulerAngles.y, 0);
-        Cameraman.transform.GetChild(0).transform.localPosition = new Vector3(0, 1.15f, Zoom);
-    }
-    void CreateRipple(int Start, int End, int Delta, float Speed, float Size, float Lifetime)
-    {
-        Vector3 forward = ripple.transform.eulerAngles;
-        forward.y = Start;
-        ripple.transform.eulerAngles = forward;
-
-        for (int i = Start; i < End; i+=Delta)
-        {
-            ripple.Emit(transform.position + ripple.transform.forward * 1.15f, ripple.transform.forward * Speed, Size, Lifetime, Color.white);
-            ripple.transform.Rotate(Vector3.up * Delta, Space.World);
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        // && !isGround.collider && Mathf.Abs(VelocityY) > 0.1f  && Mathf.Abs(VelocityY) > 0.1f
-        if(other.gameObject.layer == 4)
-        {
-            //CreateRipple(-180, 180, 2, 5, 3f, 3);
-            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
-        }
-    }
-    /*
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.layer == 4 && VelocityXZ > 0.025f && Time.renderedFrameCount % 3 == 0)
-        {
-            int y = (int)transform.eulerAngles.y;
-            CreateRipple(y-100, y+100, 3, 5f, 2.65f, 3f);
-        }
-    }
-    */
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.gameObject.layer == 4)
-        {
-            //CreateRipple(-180, 180, 2, 5, 3f, 3);
-            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
-        }
-    }
+ 
+ 
 }
